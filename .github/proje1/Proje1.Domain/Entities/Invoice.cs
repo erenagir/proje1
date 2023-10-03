0f0b973e1d6cf66ac2240efb3567ef2882fc62c5
eren ağır <agir_eren@hotmail.com>
2023-10-03 17:59:00 +0300

    fatura yerıda kaldı

---
+++ .github/proje1/Proje1.Domain/Entities/Invoice.cs
@@ -0,0 +1,14 @@
+﻿using Proje1.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proje1.Domain.Entities
+{
+    public class Invoice:AuditableEntity
+    {
+        public string MyProperty { get; set; }
+    }
+}
